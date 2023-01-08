dec755dc9344eee4f87fac10062ce2c1796e6953
chanboklee <chanboklee@naver.com>
2023-01-08 20:58:44 +0900

    Fix : Java8 -> learn Java8 API ( default method, static method )

--- src/main/java/com/lee/java8/chapter2/ChangeOfInterfaceMain.java
+++ src/main/java/com/lee/java8/chapter2/ChangeOfInterfaceMain.java
@@ -1,10 +1,27 @@
 package com.lee.java8.chapter2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
 public class ChangeOfInterfaceMain {
     public static void main(String[] args) {
         ChangeOfInterfaceImpl changeOfInterface = new ChangeOfInterfaceImpl("lee");
         changeOfInterface.printName();
         changeOfInterface.printNameUppercase();
         ChangeOfInterface.printAnything();
+
+        // 자바8 API
+        List<String> name = new ArrayList<>();
+        name.add("lee");
+        name.add("toby");
+        name.add("keesun");
+
+        // method reference
+        name.forEach(System.out::println);
+
+        name.removeIf(s -> s.startsWith("k"));
+        name.forEach(System.out::println);
+
     }
 }
